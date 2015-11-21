5f26f011ea53dae0275bfb1a99758ab6fb147576
temoty.roca <temoty.roca@goabroad.com>
2015-11-21 19:19:35 +0800

    Assets Renderer

---
+++ Core/Assets.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Handles Assets
+ *
+ * @since November 2015
+ */
+Class Assets 
+{
+	/**
+	 * Render Css Link
+	 *
+	 * @param String $filename
+	 */
+	public static function renderCss($filename)
+	{
+		$filename = 'http://'.$_SERVER['HTTP_HOST'].'/css/'.$filename;
+		echo "<link rel='stylesheet' href=$filename />";
+	}
+
+	/**
+	 * Render JS
+	 *
+	 * @param String $filename
+	 */
+	public static function renderJs($filename)
+	{
+		$filename = 'http://'.$_SERVER['HTTP_HOST'].'/js/'.$filename;
+		echo "<script src=$filename type='text/javascript'></script>";
+	}
+}
