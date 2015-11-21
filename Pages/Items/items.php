baae9a3108131ab984d447feb7e11250c3058620
temoty.roca <temoty.roca@goabroad.com>
2015-11-21 20:18:58 +0800

    Fixed Query in items.php

--- Pages/Items/items.php
+++ Pages/Items/items.php
@@ -1,99 +1,70 @@
-<?php session_start();
- include_once("../php/config.php");
+<?php 
+
+include $_SERVER['DOCUMENT_ROOT'].'/Config/DbConnection.php';
+include $_SERVER['DOCUMENT_ROOT'].'/Core/Login.php';
+include $_SERVER['DOCUMENT_ROOT'].'/Core/Assets.php';
+include $_SERVER['DOCUMENT_ROOT'].'/Core/Link.php';
+
+$db = DbConnection::connect()->getConnection(); 
+Login::sessionStart();
+
+if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />       
-  <link rel="stylesheet" href="../css/templatemo_main.css">
-  <script language="javascript" src="../javascript/confirm.js" type="text/javascript"></script>
-
+  <?php Assets::renderCss('templatemo_main.css'); ?>
+  <?php Assets::renderJs('confirm.js'); ?>
 </head>
 <body>
-  <?php include("../includes/header5.php");?>
-
+  <?php include $_SERVER['DOCUMENT_ROOT']."/includes/collapse_header.php" ;?>
       <div class="templatemo-content-wrapper">
         <div class="templatemo-content">
           <ol class="breadcrumb">
             <li><a href="item_add.php">Add Record</a></li>
-            <!--<li><a href="../addreport/inventory_add.php?area_id=<?php echo $_SESSION['area_id']; ?>">Add Inventory Report</a></li>
-            --><li class="active">Manage Items</li>
+            <li class="active">Manage Items</li>
           </ol>
           <h1>EQUIPMENT, TOOLS AND MATERIALS</h1>
 
           <div class="row">
             <div class="col-md-12">
               <div class="table-responsive">
+                <?php $result = $db->query("SELECT * FROM items join area_dept on `area_dept`.`area_id`=`Items`.`area_id` where item_status !='Deleted'"); ?>
+                
                 <table class="table table-striped table-hover table-bordered">
                   <thead>
-                    <form name="actionItem" method="post" action="">
-                      <?php
-                          $area_id = $_GET['area_id'];
-                          $_SESSION['area_id'] = $area_id;
-                          $result = mysql_query("SELECT * FROM Items where item_status!='Deleted'");                               
-                          if(mysql_num_rows($result) > 0){ 
-                            echo "
-                              <tr id='th'>
-                                <th> Select</th>
-                                <th> ID</th>
-                                <th> Area/Department</th>
-                                <th> Room Area</th>
-                                <th> Description</th>
-                                <th> Quantity</th>
-                                <th> Remarks</th>
-                              </tr>
-                            "; 
-                      ?>
+                      <?php if ($result) { ?>
+                            <tr id='th'>
+                              <th> ID</th>
+                              <th> Area/Department</th>
+                              <th> Room Area</th>
+                              <th> Description</th>
+                              <th> Quantity</th>
+                              <th> Remarks</th>
+                              <th> Action</th>
+                            </tr>
+                      <?php } ?>
                   </thead>
                   <tbody>
-                    <?php 
-                    while($row = mysql_fetch_array($result)){
-                      $_SESSION['area_id'] = $row['area_id'];
-                    ?>
-
-                    <tr>
-                      <td> <input type='checkbox' name ='checkbox[]' value= '<?php echo $row['item_id'] ; ?> '></td>
-                      <td> <?php echo $row['item_id']; ?></td>
-                      
-                      <?php
-                      $query_area = mysql_query("SELECT * FROM area_dept where area_id ='".$_SESSION['area_id']."'");
-                      if(mysql_num_rows($query_area) > 0){
-                       while($qArea = mysql_fetch_array($query_area)){
-                      ?>
-                      <td> <?php echo $qArea['area_name']; ?></td>
-                      <?php }
-                      }
-                      ?>
-                      <td> <?php echo $row['item_area']; ?></td>
-                      <td> <?php echo $row['item_description'] ?></td>
-                      <td> <?php echo $row['item_quantity']; ?></td>
-                      <td> <?php echo $row['item_status']; ?></td>
-                    </tr>         
-
-                    <?php
-                        }
-                        echo "</table>";  
-                      }
-                    echo"<input type='submit' name='update' value='Update' class='butt' onclick='setUpdateActionItem();'>";
-                    echo"<input type='submit' name='delete' value='Delete' class='butt' onClick='setDeleteActionItem();'>";
-                    ?> 
-
+                    <?php  while ($item = $result->fetch_assoc()) { ?>
+                      <tr>
+                        <td> <?php echo $item['item_id']; ?></td>
+                        <td> <?php echo $item['area_name']; ?></td>
+                        <td> <?php echo $item['item_area']; ?></td>
+                        <td> <?php echo $item['item_description'] ?></td>
+                        <td> <?php echo $item['item_quantity']; ?></td>
+                        <td> <?php echo $item['item_status']; ?></td>
+                        <td> 
+                          <a type="button" class="btn btn-primary btn-sm" href='#'>Edit</a> 
+                          <a type="button" class="btn btn-warning btn-sm" href='#'>Delete</a> 
+                        </td>
+                      </tr>  
+                    <?php } ?>
                   </tbody>
                 </table>
-                
               </div>
-              
-              <!--
-              <ul class="pagination pull-right">
-                <li class="disabled"><a href="#">&laquo;</a></li>
-                <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">2 <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">3 <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">4 <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">5 <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">&raquo;</a></li>
-              </ul> --> 
             </div>
           </div>
         </div>
@@ -104,8 +75,8 @@
           <p>Copyright &copy; Soria & Labra.  Credit: www.templatemo.com</p>
         </div>
       </footer>
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/templatemo_script.js"></script>
+      <?php Assets::renderJs('jquery.min.js'); ?>
+      <?php Assets::renderJs('bootstrap.min.js'); ?>
+      <?php Assets::renderJs('templatemo_script.js'); ?>
   </body>
 </html>
