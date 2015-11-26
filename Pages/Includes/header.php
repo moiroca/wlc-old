84e8a76ecc03e169d8aa2f88c4b31b022c4fc0d3
jtroca <john.roca@intechsive.com>
2015-11-26 21:13:12 +0800

    Area Add Form

--- Pages/Includes/header.php
+++ Pages/Includes/header.php
@@ -248,8 +248,8 @@ include $_SERVER['DOCUMENT_ROOT'].'/Core/LoggedInUser.php';
                     <ul class="dropdown-menu dropdown-user">
                         <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
                         </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
+                        <!-- <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li> -->
                         <li class="divider"></li>
                         <li><a href="<?php echo Link::createUrl('Controllers/Logout.php'); ?>"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
