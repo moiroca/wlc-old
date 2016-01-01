036106fe542d02661ed44e3dcfccca69f1db5bbc
jtroca <john.roca@intechsive.com>
2016-01-01 21:52:24 +0800

    Requisition Listing Update

--- Pages/Requisitions/myrequisitions.php
+++ Pages/Requisitions/myrequisitions.php
@@ -33,7 +33,9 @@ if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
           <thead>
               <tr id='th'>
                   <th> Control Identifier </th>
-                  <th> GSD Officer</th>
+                  <th> Noted By Department Head</th>
+                  <th> Type</th>
+                  <th> Comptroller</th>
                   <th> President</th>
                   <th> Purpose </th>
                   <th> Status </th>
@@ -42,19 +44,56 @@ if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
           <tbody>
               <?php $userObj = new User(); ?>
               <?php if ($result && 0 != $result->num_rows) { ?>
+                  <?php $ind = 0; $firstItem = ''; ?>
                   <?php  while ($item = $result->fetch_assoc()) { ?>
+                    <?php if (0 == $ind): ?>
+                        <?php $stocksRepo = new Stocks(); ?>
+                        <?php $itemsInRequisition = $stocksRepo->getStockByRequisitionId($item['id']); ?>
+                        <?php $firstItem = $itemsInRequisition->fetch_assoc(); ?>
+                    <?php endif ?>
                     <tr>
                       <td> 
                           <a title="View Details Of Requisition" href="<?php echo Link::createUrl('Pages/Requisitions/requisition.php?control_identifier='.$item['control_identifier']); ?>"><?php echo $item['control_identifier']; ?></a>
                       </td>
-                      <td> 
-                          <?php if ($item['gsd_officer_id']): ?>
-                              <?php 
-                                $user = $userObj->getAll(['*'], ['id' => $item['gsd_officer_id']])->fetch_assoc();
+                      <td>
+                          <?php if ($item['department_head_id']): ?>
+                              <?php
+                                $user = $userObj->getAll(['*'], ['id' => $item['department_head_id']])->fetch_assoc();
                                 echo $user['lastname'].', '.$user['firstname'];
                               ?>
                           <?php else: ?>
-                             <label class="label label-warning"> Not Yet Approved By GSD Officer</label>
+                             <label class="label label-warning"> Not Yet Noted By Department Head</label>
+                          <?php endif ?>
+                      </td>
+                      <td> 
+                          <?php if ($firstItem && $firstItem['stock_type'] == Constant::ITEM_MATERIAL_EQUIPMENT): ?>
+                              <?php if ($item['gsd_officer_id']): ?>
+                                  <?php 
+                                    $user = $userObj->getAll(['*'], ['id' => $item['gsd_officer_id']])->fetch_assoc();
+                                    echo '<b>GSD Officer: </b>'.$user['lastname'].', '.$user['firstname'];
+                                  ?>
+                              <?php else: ?>
+                                 <label class="label label-warning"> Not Yet Approved By GSD Officer</label>
+                              <?php endif ?>
+                          <?php else: ?>
+                              <?php if ($item['property_custodian_id']): ?>
+                                  <?php 
+                                    $user = $userObj->getAll(['*'], ['id' => $item['property_custodian_id']])->fetch_assoc();
+                                    echo '<b>Property Custodian: </b>'. $user['lastname'].', '.$user['firstname'];
+                                  ?>
+                              <?php else: ?>
+                                 <label class="label label-warning"> Not Yet Verified By Property Custodian</label>
+                              <?php endif ?>
+                          <?php endif ?>
+                      </td>
+                      <td>
+                          <?php if ($item['comptroller_id']): ?>
+                              <?php
+                                $user = $userObj->getAll(['*'], ['id' => $item['comptroller_id']])->fetch_assoc();
+                                echo $user['lastname'].', '.$user['firstname'];
+                              ?>
+                          <?php else: ?>
+                             <label class="label label-warning"> Not Yet Approved By Comptroller</label>
                           <?php endif ?>
                       </td>
                       <td> 
@@ -69,15 +108,23 @@ if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
                       </td>
                       <td> <?php echo $item['purpose']; ?></td>
                       <td>
-                          <?php if ($item['status'] == Constant::REQUISITION_APPROVED): ?>
-                              <label class="label label-success"><?php echo $item['status']; ?></label>  
-                          <?php elseif ($item['status'] == Constant::REQUISITION_DECLINED): ?>
-                              <label class="label label-danger"><?php echo $item['status']; ?></label>  
-                          <?php else: ?>
-                              <label class="label label-info"><?php echo $item['status']; ?></label>  
-                          <?php endif ?>
+                          <?php 
+
+                            $item['stock_type'] = isset($firstItem['stock_type']) ? $firstItem['stock_type'] : ''; 
+                            
+                            if (RequisitionUtility::isRequisitionApproved($item)) {
+                          ?>
+                            <label class="label label-success"><?php echo $item['status']; ?></label>
+                          <?php  
+                            } else {
+                          ?>
+                            <label class="label label-warning"><?php echo $item['status']; ?></label>  
+                          <?php
+                            }
+                          ?>
                       </td>
                     </tr>  
+                    <?php $ind++; ?>
                   <?php } ?>
               <?php } else { ?>
                     <tr>
--- Utilities/RequisitionUtility.php
+++ Utilities/RequisitionUtility.php
@@ -34,4 +34,34 @@ class RequisitionUtility
 			Constant::REQUISITION_FINISHED,
 		];
 	}
+
+	/**
+	 * Is Approved Requisition
+	 * 
+	 * @param Array $requisition
+	 * @return Boolean
+	 */
+	public static function isRequisitionApproved($requisition)
+	{	
+		$isApproved = false;
+
+		if ($requisition['president_id']) {
+			
+			if ($requisition['department_head_id']) {
+				
+				if ($requisition['comptroller_id']) {
+
+					if ($requisition['stock_type'] == Constant::ITEM_MATERIAL_EQUIPMENT && $requisition['gsd_officer_id']) {
+						
+						$isApproved = ($requisition['treasurer_id'] && $requisition['type'] == Constant::REQUISITION_JOB) ? true : false;
+					} else if ($requisition['stock_type'] == Constant::ITEM_OFFICE_SUPPLY && $requisition['property_custodian_id']) {
+						
+						$isApproved = ($requisition['treasurer_id'] && $requisition['type'] == Constant::REQUISITION_JOB) ? true : false;
+					}
+				}		
+			}
+		} 
+
+		return $isApproved;
+	}
 }
