55597c485b4a854d5ad4378f7af6cb7edd3e9454
jtroca <john.roca@intechsive.com>
2016-01-01 23:43:39 +0800

    Item Requisition Update

--- Pages/Requisitions/Items/listing.php
+++ Pages/Requisitions/Items/listing.php
@@ -5,6 +5,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/Core/Loader.php';
 Login::sessionStart();
 
 $requisitions = new Requisitions();
+$userObj = new User();
 
 if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
 
@@ -26,7 +27,6 @@ if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
   <div class="row">
     <div class="col-md-12">
       <div class="table-responsive">
-
         <?php 
             if (Login::getUserLoggedInType() == Constant::USER_PRESIDENT) {
                 $result = $requisitions->getAllRequesitionForApprovalByPresident(Constant::REQUISITION_ITEM);  
@@ -34,7 +34,6 @@ if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
                 $result = $requisitions->getAllRequesition(Constant::REQUISITION_ITEM);
             }
         ?>
-
         <?php if (isset($_SESSION['record_successful_added'])) { ?>
         <?php unset($_SESSION['record_successful_added']); ?>
             <div class="alert alert-success">
@@ -54,91 +53,109 @@ if (!Login::isLoggedIn()) { Login::redirectToLogin(); }
         <table class="table table-striped table-hover table-bordered">
           <thead>
               <tr id='th'>
-                <?php if (Login::getUserLoggedInType() == Constant::USER_PRESIDENT): ?>
-                  <th> Control Identifier </th>
-                  <th> Approved By </th>
-                  <th> Purpose </th>
-                  <th> Action </th>
-                <?php elseif (Login::getUserLoggedInType() == Constant::USER_GSD_OFFICER): ?>
                   <th> Control Identifier </th>
                   <th> Requester Name </th>
                   <th> Purpose </th>
-
+                  <th> Department Head Details</th>
+                  <th> <?php echo Constant::ITEM_MATERIAL_EQUIPMENT.'/ '.Constant::ITEM_OFFICE_SUPPLY; ?></th>
+                  <th> Approved By Comptroller </th>
+                  <th> Approved By President </th>
                   <th> Status </th>
                   <th> Action </th>
-                <?php endif ?>
               </tr>
           </thead>
           <tbody>
             <input type="hidden" id="declined_item_requisition_url" value="<?php echo Link::createUrl('Controllers/DeclineRequisition.php'); ?>" />
-            <?php if (Login::getUserLoggedInType() == Constant::USER_PRESIDENT): ?>
-              <input type="hidden" id="approve_item_requisition_url" value="<?php echo Link::createUrl('Controllers/ApproveRequisitionByPresident.php'); ?>" />
-              <?php if ($result && 0 != $result->num_rows) { ?>
-                  <?php  while ($item = $result->fetch_assoc()) { ?>
-                    <tr data-id="<?php echo $item['requisition_id']; ?>" data-type='<?php echo Constant::REQUISITION_ITEM; ?>'>
-                      <td> 
-                          <a title="View Details Of Requisition" href="<?php echo Link::createUrl('Pages/Requisitions/requisition.php?control_identifier='.$item['requisition_control_identifier']); ?>"><?php echo $item['requisition_control_identifier']; ?></a>
+            <input type="hidden" id="approve_item_requisition_url" value="<?php echo Link::createUrl('Controllers/ApproveRequisitionByPresident.php'); ?>" />
+            <?php if ($result && 0 != $result->num_rows) { ?>
+                <?php  while ($item = $result->fetch_assoc()) { ?>
+                  <?php // var_dump($item); die(); ?>
+                  <tr data-id="<?php echo $item['requisition_id']; ?>" data-type='<?php echo Constant::REQUISITION_ITEM; ?>'>
+                    <td> 
+                        <a title="View Details Of Requisition" href="<?php echo Link::createUrl('Pages/Requisitions/requisition.php?control_identifier='.$item['requisition_control_identifier']); ?>"><?php echo $item['requisition_control_identifier']; ?></a>
+                    </td>
+                    <td> <?php echo RequesterUtility::getFullName($item); ?></td>
+                    <td> <?php echo $item['requisition_purpose']; ?></td>
+                    <td>
+                        <?php
+                            $user = $userObj->getAll(['*'], ['id' => $item['requisition_department_head_id']])->fetch_assoc();
+                        ?>
+                        <?php if (!is_null($user)) { ?>
+                            <?php echo $user['lastname'].', '.$user['firstname']; ?>
+                        <?php } else { ?>
+                            <label class='label label-info'>Not Available</label>
+                        <?php } ?>
+                    </td>
+                    <td>
+                        <?php
+                            $stocksRepo = new Stocks();
+                            $itemsInRequisition = $stocksRepo->getStockByRequisitionId($item['requisition_id']);
+                            $firstItem = $itemsInRequisition->fetch_assoc();
+
+                        ?>
+
+                        <?php if ($firstItem && $firstItem['stock_type'] == Constant::ITEM_MATERIAL_EQUIPMENT): ?>
+                              <?php
+                                  $user = $userObj->getAll(['*'], ['id' => $item['requisition_gsd_officer_id']])->fetch_assoc();
+                              ?>
+                              <?php if (!is_null($user)) { ?>
+                                  <b>GSD Officer Name: </b> 
+                                  <?php echo $user['lastname'].', '.$user['firstname']; ?>
+                              <?php } else { ?>
+                                  <label class='label label-info'>Not Available</label>
+                              <?php } ?> 
+                        <?php else: ?>
+                              <?php
+                                  $user = $userObj->getAll(['*'], ['id' => $item['requisition_gsd_officer_id']])->fetch_assoc();
+                              ?>
+                              <?php if (!is_null($user)) { ?>
+                                  <b>Property Custodian Name: </b> 
+                                  <?php echo $user['lastname'].', '.$user['firstname']; ?>
+                              <?php } else { ?>
+                                  <label class='label label-info'>Not Available</label>
+                              <?php } ?>
+                        <?php endif ?>
+                    </td>
+                    <td>
+                        <?php
+                            $user = $userObj->getAll(['*'], ['id' => $item['requisition_comptroller_id']])->fetch_assoc();
+                        ?>
+                        <?php if (!is_null($user)) { ?>
+                            <b>Comptroller Name: </b> 
+                            <?php echo $user['lastname'].', '.$user['firstname']; ?>
+                        <?php } else { ?>
+                            <label class='label label-info'>Not Available</label>
+                        <?php } ?>
+                    </td>
+                    <td>
+                        <?php
+                            $user = $userObj->getAll(['*'], ['id' => $item['requisition_president_id']])->fetch_assoc();    
+                        ?>
+                        <?php if (!is_null($user)) { ?>
+                            <b>President Name: </b> 
+                            <?php echo $user['lastname'].', '.$user['firstname']; ?>
+                        <?php } else { ?>
+                            <label class='label label-info'>Not Available</label>
+                        <?php } ?>
+                    </td>
+                    <td>
+                        
+                    </td>
+                    <td> 
+                        <a href="javascript:void(0)" class='btn btn-large btn-primary approve_item_by_president_btn'> <i class='fa fa-thumbs-up'></i> Approve</a>
+                        <a href="javascript:void(0)" class='btn btn-sm btn-warning decline_requisition'> <i class='fa fa-thumbs-down'></i> Decline</a>
+                    </td>
+                  </tr>  
+                <?php } ?>
+            <?php } else { ?>
+                  <tr>
+                      <td colspan=7>
+                          <div class="alert alert-info">
+                              There are no items found.
+                          </div>
                       </td>
-                      <td> <?php echo '<b>'.$item['approver_type'].'</b>'.': '.RequesterUtility::getFullName($item); ?></td>
-                      <td> <?php echo $item['requisition_purpose']; ?></td>
-                      <td>
-                          <?php if ($item['requisition_status'] != Constant::REQUISITION_APPROVED) { ?> 
-                            <a href="javascript:void(0)" class='btn btn-large btn-primary approve_item_by_president_btn'> <i class='fa fa-thumbs-up'></i> Approve</a>
-                            <a href="javascript:void(0)" class='btn btn-sm btn-warning decline_requisition'> <i class='fa fa-thumbs-down'></i> Decline</a>
-                          <?php } ?>
-                      </td>
-                    </tr>  
-                  <?php } ?>
-              <?php } else { ?>
-                    <tr>
-                        <td colspan=7>
-                            <div class="alert alert-info">
-                                There are no items found.
-                            </div>
-                        </td>
-                    </tr>
-              <?php } ?>
-            <?php elseif (Login::getUserLoggedInType() == Constant::USER_GSD_OFFICER): ?>
-              <input type="hidden" id="approve_item_requisition_url" value="<?php echo Link::createUrl('Controllers/ApproveRequisitionByGSDOfficer.php'); ?>" />
-              <?php if ($result && 0 != $result->num_rows) { ?>
-                  <?php  while ($item = $result->fetch_assoc()) { ?>
-                    <tr data-id="<?php echo $item['requisition_id']; ?>" data-type='<?php echo Constant::REQUISITION_ITEM; ?>'>
-                      <td> 
-                          <a title="View Details Of Requisition" href="<?php echo Link::createUrl('Pages/Requisitions/requisition.php?control_identifier='.$item['requisition_control_identifier']); ?>"><?php echo $item['requisition_control_identifier']; ?></a>
-                      </td>
-                      <td> <?php echo RequesterUtility::getFullName($item); ?></td>
-                      <td> <?php echo $item['requisition_purpose']; ?></td>
-                      <td> 
-                          <?php if ($item['requisition_status'] == Constant::REQUISITION_APPROVED): ?>
-                              <label class="label label-success"><?php echo $item['requisition_status']; ?></label>  
-                          <?php elseif ($item['requisition_status'] == Constant::REQUISITION_DECLINED): ?>
-                              <label class="label label-danger"><?php echo $item['requisition_status']; ?></label>  
-                          <?php else: ?>
-                              <label class="label label-info"><?php echo $item['requisition_status']; ?></label>  
-                          <?php endif ?>
-                      </td>
-                      <?php if ($item['requisition_status'] != Constant::REQUISITION_APPROVED && $item['requisition_status'] != Constant::REQUISITION_DECLINED) : ?> 
-                        <td>
-                              <a href="javascript:void(0)" class='btn btn-large btn-primary approve_item_by_gsd_officer'> <i class='fa fa-thumbs-up'></i> Approve</a>
-                              <a href="javascript:void(0)" class='btn btn-sm btn-warning decline_requisition'> <i class='fa fa-thumbs-down'></i> Decline</a>
-                        </td>    
-                      <?php else: ?>
-                        <td>
-                            <span class='label label-info'> <i class='fa fa-info'></i> There is no action available.</span>
-                        </td>
-                      <?php endif ?>
-                    </tr>  
-                  <?php } ?>
-              <?php } else { ?>
-                    <tr>
-                        <td colspan=7>
-                            <div class="alert alert-info">
-                                There are no items found.
-                            </div>
-                        </td>
-                    </tr>
-              <?php } ?>
-            <?php endif ?>
+                  </tr>
+            <?php } ?>
           </tbody>
         </table>
         <span>
--- Repositories/Requisitions.php
+++ Repositories/Requisitions.php
@@ -22,13 +22,37 @@ Class Requisitions extends Base
                 `users`.`firstname` as user_firstname,
                 `users`.`middlename` as user_middlename,
                 `users`.`lastname` as user_lastname,
-                `$this->table`.`status` as requisition_status
+                `$this->table`.`status` as requisition_status,
+                `$this->table`.`id` as requisition_id,
+                `$this->table`.`purpose` as requisition_purpose,
+                `$this->table`.`type` as requisition_type,
+                `$this->table`.`requester_id` as requisition_requester_id,
+                `$this->table`.`control_identifier` as requisition_control_identifier,
+                `$this->table`.`datetime_approveddeclined_by_president` as requisition_datetime_approveddeclined_by_president,
+                `$this->table`.`datetime_approveddeclined_by_gsd_officer` as requisition_datetime_approveddeclined_by_gsd_officer,
+                `$this->table`.`datetime_approveddeclined_by_comptroller` as requisition_datetime_approveddeclined_by_comptroller,
+                `$this->table`.`datetime_approveddeclined_by_property_custodian` as requisition_datetime_approveddeclined_by_property_custodian,
+                `$this->table`.`datetime_approveddeclined_by_department_head` as requisition_datetime_approveddeclined_by_department_head,
+                `$this->table`.`datetime_approveddeclined_by_treasurer` as requisition_datetime_approveddeclined_by_treasurer,
+                `$this->table`.`datetime_added` as requisition_datetime_added,
+                `$this->table`.`status` as requisition_status,
+                `$this->table`.`president_id` as requisition_president_id,
+                `$this->table`.`gsd_officer_id` as requisition_gsd_officer_id,
+                `$this->table`.`department_head_id` as requisition_department_head_id,
+                `$this->table`.`comptroller_id` as requisition_comptroller_id,
+                `$this->table`.`property_custodian_id` as requisition_property_custodian_id,
+                `$this->table`.`treasurer_id` as requisition_treasurer_id,
+                `areas`.`name` as requisition_area_name
               FROM 
                 $this->table
               JOIN 
                 users 
               ON 
                 `users`.`id`=`$this->table`.`requester_id`
+              JOIN
+                `areas`
+              ON
+                `$this->table`.`area_id` = `areas`.`id`
               WHERE
                 `$this->table`.`type` = '".$type."'";
 
